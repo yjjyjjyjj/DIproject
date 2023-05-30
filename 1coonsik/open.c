71bc8c6437f97ca21facc72200f246188b8e404e
yjjyjjyjj <thfw66@naver.com>
2023-05-30 16:41:03 +0900

    테스트

---
+++ 1coonsik/open.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("╭");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╮\n");
+    printf("│                                                                                                        │\n");
+    printf("│                                                                                                        │\n");
+    printf("│\t\t\t\x1b[33m     :^^:    :   ^                           .::.  .                             \x1b[0m│\n");
+    printf("│\t\t\t\x1b[33m     .^~7~.  :7   7 :^:  ~:.^^^. ~.    .!^^~! :!!^. !.                           \x1b[0m│\n");
+    printf("│\t\t\t\x1b[33m    .^~7~^ :~~^  7~~.!: !^!~.!^ ?.    :7::~? ~!~~:^J.                            \x1b[0m│\n"); // 50
+    printf("│\t\t\t\x1b[33m    ^~^~~^:.::::::7: 7: !^ :::  7.     :^~.. ^~!!..!                             \x1b[0m│\n");
+    printf("│\t\t\t\x1b[33m    ^^ ^^  .::::?:.~~^  !^^^^^^.!     :^!7^^: ?^::~7                             \x1b[0m│\n");
+    printf("│\t\t\t\x1b[33m    :!^^^^.     ~.      ..      .     ....... ^^^^~^                             \x1b[0m│\n");
+    printf("│                                                                                                        │\n");
+    printf("│                                                                                                        │\n");
+    printf("│                                                                                                        │\n");
+    printf("╰");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╯\n");
+
+    usleep(600000);
+    system("clear");
+
+    printf("╭");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╮\n");
+    printf("│                                                                                                        │\n");
+    printf("│\t\t\x1b[34m      .....            :                                                                 \x1b[0m│\n");
+    printf("│\t\t\x1b[34m     .^~~~~.     7.   :?         ^.  ...    .       ::::::. .^~~~:   ^:                  \x1b[0m│\n");
+    printf("│\t\t\x1b[34m     .~^~Y!^.   ~J.   :? :!!!.   J:.!~^~!. .?      :J^^^:?7 .^!!!^:..?^                 \x1b[0m │\n");
+    printf("│\t\t\x1b[34m     .~~~7~~: :~~~!~  :?^7. :?   J:^?:.:?^ :J      :J::::?7 .7~^!~.~~5:                 \x1b[0m │\n");
+    printf("│\t\t\x1b[34m    :~~^^^^^^: ...... .^7~  :J   J: .^^^.  :J       ::!~::. .7~~7~   7.                 \x1b[0m │\n"); // 70
+    printf("│\t\t\x1b[34m    .!:..!7.:. ^~~~~~!7 .!!~7:   J::^^^^^^.:J      .. ?!....  ?~~^^^!7                  \x1b[0m │\n");
+    printf("│\t\t\x1b[34m     ?~..:^..        ^J   .:.    !.:::::::..~     .~~~!!~~~^  J^::::77                  \x1b[0m │\n");
+    printf("│\t\t\x1b[34m     :~^^^^^:.       .^                                       .^:::::.                  \x1b[0m │\n");
+    printf("│                                                                                                        │\n");
+    printf("│                                                                                                        │\n");
+
+    printf("╰");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╯\n");
+
+    usleep(600000);
+    system("clear");
+
+    printf("╭");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╮\n");
+    printf("│\x1b[32m        ......                 .:                                                                       \x1b[0m│\n");
+    printf("│\x1b[32m       ^~!!77!:        ?~      7J             :.                                     .!!!!!~.     ~.    \x1b[0m│\n");
+    printf("│\x1b[32m      .^^^^^^^^^.     .5:      7?    ::       J^   :~!!~.    :~         .?7777777?:  .::^^^^:.   .Y.    \x1b[0m│\n");
+    printf("│\x1b[32m      .^^^:7G~^^      7P:      ??  !?77?^     Y! .7?~::^J~   !Y         ^P:.....:P~  :~!777!~:...^5.    \x1b[0m│\n");
+    printf("│\x1b[32m      .^~!7JY7!!^  :!77!?7!.   ?7 J7.  :5:    5! ~5.    7J   7Y         ^P......:G^   ~77!77..!77Y5     \x1b[0m│\n");
+    printf("│\x1b[32m      .~^:.   .^:  ::    .:.   ?!^5     Y~    5!  ~?7!!77:   7J         .J?77777?J:  ^P^  .57    !Y     \x1b[0m│\n");
+    printf("│\x1b[32m    .~!!!!!!!7!~!!.  ....      :.~5    :5:    5!    ....     7J             ^?        ~7!!7!.    ~7     \x1b[0m│\n");
+    printf("│\x1b[32m     .^^:::.?Y::::. ~777777777J.  7J~::Y7     5! ..........  7?             !J         ~?77777777?:     \x1b[0m│\n");
+    printf("│\x1b[32m      J!    ^7               :P:   :!77^      5^:!!77777777: ?7        ^!!!~J5!!!!!~.  JJ       :P^     \x1b[0m│\n");
+    printf("│\x1b[32m      7Y:::::::::.           :P:              !.             ^:        :^^^^^^^^^^^:.  ~5~~~~~~~?5.     \x1b[0m│\n");
+    printf("│\x1b[32m      .!!!!!!!!~~:           .!.                                                        ^~^^^^^^:.      \x1b[0m│\n");
+
+    printf("╰");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╯\n");
+    sleep(1);
+
+
+    system("clear");
+    printf("╭");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╮\n");                                                                                                
+    printf("│                                      ^5GBBG5!::::::::..                                                │\n");
+    printf("│                                    .B&J^::^?GBGGGPGGGGGBBBGY7^.                                        │\n");
+    printf("│                                   7&&^..................:^~?5G#B5!~!7!^.                               │\n");
+    printf("│                                .5&B7...........................^?PPYY5G&G.                             │\n");
+    printf("│                              .P&G^.....................................^&&                             │\n");
+    printf("│                             J@B^........................................5@:                            │\n");
+    printf("│                           .#&7..........................................!@G                            │\n");
+    printf("│                          :@#:..........:5&&5.............................~@B                           │\n");
+    printf("│                         .@#............:#@@B..........:?P5^...............7@Y                          │\n");
+    printf("│                         B@^...........:..:^:^^^:......!@@@G................G@.                         │\n");
+    printf("│                        ^@Y...........:G&PGBBGGGBBBP?:..^7!:................~@Y                         │\n");
+    printf("│                        Y@~.............#@!       .!P&PJY!..................:&#                         │\n");
+    printf("│                        G@:...........JB@@!...::     J@G?~...................#&                         │\n");
+    printf("│                        P@^...........:^:?GBBBB#B5?75&@G^...................:&#                         │\n");
+    printf("│                        ~@J.....................^7JJ?^~57...................!@J                         │\n");
+    printf("│                      .:~&@^................................................B@.                         │\n");
+    printf("│                  .?GBBGPP&&~..............................................J@?                          │\n");
+    printf("│                .G&G!:....:G@5:...........................................?@5                           │\n");
+    printf("│               ~&B:.........!B&P~........................................5@Y                            │\n");
+    printf("│           ^&5!@@P5PP5J!^.....~5##P7:..................................7##^                             │\n");
+    printf("│       .G&##&&##BGGGGB#&&#5~.....~YB#BPJ!^:..............:^~7JY55PPPPG&@&?:.                            │\n");
+    printf("│       Y@GYYJJJJJJJJJJJJYP@@7.......:^?5GB#BBG5YJ?7!!!YGB#BGPYJ?77!!!!77?5GB#P^                         │\n");
+    printf("│       ?@GJJJJJJJJJJJJJJJG@P..............:^~!?JY5PPGG57^:..................^Y&B.                       │\n");
+    printf("│       :@#JJJJJJJJJJJJJJG@P...................................................^@G                       │\n");
+    printf("│       :#@PJJJJJJJJJJJJP@#~^^::::..............................................&&                       │\n");
+    printf("│       JYB@BJJJJJJJJJJ5@@YJJJJJJ??7!!~^^::....................................Y@?                       │\n");
+    printf("│        .G&@&PJJY5GBB#@@5JJJJJJJJJJJJJJJJJ??7!~^::..............:^^~~~^^^~~75##!                        │\n");
+    printf("│         . .?#&&&&#BBGG5JJJJJJJJJJJJJJJJJJJJJJJJJJ?7!~^:......G&BGPPPPPGGGPY7:                          │\n");
+    printf("│             ?@#5JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ???~^?@J                                        │\n");
+    printf("│            .&&JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ&@!                                        │\n");
+    printf("│            .&&JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJY@@&&#5:                                    │\n");
+    printf("│             7@#YJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJBBJY5#@5                                   │\n");
+    printf("│              ^B&&BPYYJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJB@~                                  │\n");
+    printf("│                .!5G##&###########BBBGP5YJJJJJJJJJJJJJJJJJJJJJJJJJJB@~                                  │\n");
+    printf("│                      ..::^^^~~~~~!7J5PB#&&#G5YJJJJJJJJJJJJJJJJJJJG@G         !!!!!       !!!!!         │\n");
+    printf("│                                         .^?PB#&##BGPP55YYYYYY5PB&&J        !!!   !!!   !!!   !!!       │\n");
+    printf("│                                               .^!?YPGBB######BGY~            !!!!!       !!!!!         │\n");
+    printf("│                                                                                                        │\n");
+    printf("│                                            잘자란 춘식이...                                            │\n");
+    printf("│                         어느날 바람에 날아온 잡지가 춘식이의 얼굴을 강타하였다.                        │\n");
+    printf("│                                     툭 떨어진 잡지를 보게된 춘식이                                     │\n");
+    printf("│                              새로운 세상을 보게된 춘식이는 모험을 떠난다....                           │\n");
+                                                                                                
+    printf("╰");
+    for (int i = 0; i < 104; i++)
+    {
+        printf("─");
+    }
+    printf("╯\n");
+    usleep(600000);                                                                                               
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+return 0;
+}
